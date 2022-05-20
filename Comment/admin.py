808351852e8dac969b30d2f3914b6245ac90570b
arsham <aqaarsham@gmail.com>
2022-05-20 12:52:30 +0430

    create model and admin

--- Comment/admin.py
+++ Comment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Comment.models import Comments
 
-# Register your models here.
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'comment',)
+
--- Comment/models.py
+++ Comment/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from Post.models import Blog
 
-# Create your models here.
+
+class Comments(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        verbose_name=_("User")
+    )
+    post = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE,
+        verbose_name=_("Blog")
+    )
+    comment = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name=_("Comment")
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+        ordering = ['-created_at']
