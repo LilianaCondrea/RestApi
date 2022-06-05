52eba1bdc4df5627da5d456514b65a2068b62ea0
arsham <aqaarsham@gmail.com>
2022-06-06 01:45:14 +0430

    clean imported and update depends

--- code/Account/api/urls.py
+++ code/Account/api/urls.py
@@ -1,22 +1,24 @@
 from django.urls import path
-from dj_rest_auth import views
+from dj_rest_auth.views import (
+    LogoutView, LoginView,PasswordChangeView,)
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
 from rest_framework_simplejwt.views import TokenVerifyView
-from .views import UserListView, UserDetailView, ProfileUserView
+from .views import (
+    UserListView, UserDetailView,
+    ProfileUserView
+)
 
 app_name = "Account"
 
 urlpatterns = [
-
-    path('login/', views.LoginView.as_view(), name='rest_login'),
-    path('logout/', views.LogoutView.as_view(), name='rest_logout'),
+    path('login/', LoginView.as_view(), name='rest_login'),
+    path('logout/', LogoutView.as_view(), name='rest_logout'),
     path('register/', RegisterView.as_view(), name='rest_register'),
-    path('password/change/', views.PasswordChangeView.as_view(), name='rest_password_change'),
+    path('password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
     path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/', UserListView.as_view(), name='user_list'),
     path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
     path('user/profile/<int:pk>/', ProfileUserView.as_view(), name='user_profile'),
-
 ]
