a50b873d481b0a8e1b47df1cf8d8387eaf84444f
arsham <aqaarsham@gmail.com>
2022-06-17 19:33:00 +0430

    add publish action to panel

--- code/Post/admin.py
+++ code/Post/admin.py
@@ -6,6 +6,12 @@ from .models import Blog, Category
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('content', 'user', 'category',)
     prepopulated_fields = {'slug': ('content',)}
+    actions = ['make_published', ]
+
+    @admin.action(description='Publish selected Blogs')
+    def make_published(self, request, queryset):
+        queryset.update(status='1')
+        self.message_user(request, 'Selected Blogs are published')
 
 
 admin.site.register(Category)
