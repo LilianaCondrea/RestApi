a56b5e39277146969db2edd3aef5da375e08690c
arsham <aqaarsham@gmail.com>
2022-07-09 20:30:05 +0430

    clear test with one assert equal

--- code/Comment/tests/test_views.py
+++ code/Comment/tests/test_views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from rest_framework.reverse import reverse
+from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-
-from Comment.api.serializers import CommentListSerializer, CommentCreateUpdateDeleteSerializer
-from Comment.models import Comments
+from Comment.models import Comments, Reply_Comment
 from Post.models import Blog, Category
+from Comment.api.serializers import (
+    CommentListSerializer, ReplyCommentSerializer,
+    CommentCreateUpdateDeleteSerializer
+)
 
 
 class CommentViewApiTest(APITestCase):
@@ -17,7 +20,6 @@ class CommentViewApiTest(APITestCase):
             password='Seconduser!123',
         )
         self.jwt_user1 = RefreshToken.for_user(self.user1)
-        # ----------------------------------
         self.user2 = get_user_model().objects.create_user(
             username='seconduser',
             email='seconduser20@email.com',
@@ -25,7 +27,6 @@ class CommentViewApiTest(APITestCase):
             password='seconduser!123',
         )
         self.jwt_user2 = RefreshToken.for_user(self.user2)
-        # ----------------------------------
         self.category = Category.objects.create(
             title='Django Test'
         )
@@ -41,57 +42,276 @@ class CommentViewApiTest(APITestCase):
             post=self.blog,
             comment='Perfect Blog !',
         )
-        # ----------------------------------
+        self.reply = Reply_Comment.objects.create(
+            user=self.user2,
+            comment=self.comment,
+            reply_text="i agree its great"
+        )
 
-    def test_comment_list(self):
+    def test_comment_list_status_code(self):
         response = self.client.get(
             reverse('Comment:list_comment', kwargs={'pk': self.blog.pk})
         )
-        serializer = CommentListSerializer(instance=Comments.objects.filter(post=self.blog), many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_comment_list_content(self):
+        response = self.client.get(
+            reverse('Comment:list_comment', kwargs={'pk': self.blog.pk})
+        )
+        serializer = CommentListSerializer(
+            instance=Comments.objects.filter(post=self.blog),
+            many=True
+        )
         self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Perfect Blog !')
+
+    def test_comment_list_not_found(self):
+        response = self.client.get(
+            reverse('Comment:list_comment', kwargs={'pk': 0})
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_comment_create(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token))
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token)
+        )
         path = reverse('Comment:create_comment', kwargs={'pk': self.blog.pk})
         serializer = CommentCreateUpdateDeleteSerializer(
             data={'comment': 'Awsome Blog !'}
         )
         serializer.is_valid(raise_exception=True)
         response = self.client.post(path, serializer.data)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['comment'], 'Awsome Blog !')
 
-    def test_comment_update(self):
-        path = reverse(
-            'Comment:update_delete_cm', kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+    def test_comment_create_more_than_one(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token)
         )
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token))
+        path = reverse('Comment:create_comment', kwargs={'pk': self.blog.pk})
+        serializer = CommentCreateUpdateDeleteSerializer(
+            data={'comment': 'Awsome Blog !'}
+        )
+        serializer.is_valid(raise_exception=True)
+        for _ in range(2):
+            response = self.client.post(
+                path=path,
+                data=serializer.data
+            )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_comment_create_authenticate(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION=None
+        )
+        path = reverse('Comment:create_comment', kwargs={'pk': self.blog.pk})
+        serializer = CommentCreateUpdateDeleteSerializer(
+            data={'comment': 'Awsome Blog !'}
+        )
+        serializer.is_valid(raise_exception=True)
+        response = self.client.post(path, serializer.data)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_comment_update_status_code(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token)
+        )
+        path = reverse(
+            'Comment:update_comment',
+            kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+        )
         serializer = CommentCreateUpdateDeleteSerializer(
             data={'comment': 'Perfect Blog Edited!'}
         )
         serializer.is_valid(raise_exception=True)
         response = self.client.put(path, data=serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, serializer.data)
-        self.assertNotEqual(self.comment, response.data)
-        self.assertContains(response, ' Edited!')
-
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token))
-        response = self.client.put(path, data=serializer.data)
-        self.assertEqual(response.status_code, 403)
-
-    def test_comment_delete(self):
-        path = reverse(
-            'Comment:update_delete_cm', kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+    def test_comment_update_content(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token)
         )
+        path = reverse(
+            'Comment:update_comment',
+            kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+        )
+        serializer = CommentCreateUpdateDeleteSerializer(
+            data={'comment': 'Perfect Blog Edited!'}
+        )
+        serializer.is_valid(raise_exception=True)
+        response = self.client.put(path, data=serializer.data)
+        self.assertEqual(response.data, serializer.data)
+        self.assertContains(response, 'Edited')
 
+    def test_comment_update_invalid_data(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token)
+        )
+        path = reverse(
+            'Comment:update_comment',
+            kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+        )
+        response = self.client.put(path, {"comment": "", })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_comment_update_authenticate(self):
+        path = reverse(
+            'Comment:update_comment',
+            kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+        )
+        serializer = CommentCreateUpdateDeleteSerializer(
+            data={'comment': 'Perfect Blog Edited!'}
+        )
+        serializer.is_valid(raise_exception=True)
+        response = self.client.put(path, data=serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_comment_delete_status_code(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token)
+        )
+        path = reverse(
+            'Comment:delete_comment',
+            kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+        )
+        response = self.client.delete(path)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_comment_delete_forbidden(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token)
+        )
+        path = reverse(
+            'Comment:delete_comment',
+            kwargs={'slug': self.blog.slug, 'pk': self.comment.pk}
+        )
+        response = self.client.delete(path)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_reply_status_code(self):
+        path = reverse(
+            'Comment:replies',
+            kwargs={'pk': self.comment.pk}
+        )
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_reply_content(self):
+        path = reverse(
+            'Comment:replies',
+            kwargs={'pk': self.comment.pk}
+        )
+        serializer = ReplyCommentSerializer(
+            Reply_Comment.objects.filter(comment=self.comment),
+            many=True
+        )
+        response = self.client.get(path)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_reply_create(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token)
+        )
+        path = reverse(
+            'Comment:reply_create',
+            kwargs={'pk': self.comment.pk}
+        )
+        data = {"reply_text": "Great Blog anyway"}
+        response = self.client.post(path, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_reply_create_more_than_one(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token)
+        )
+        path = reverse(
+            'Comment:reply_create',
+            kwargs={'pk': self.comment.pk}
+        )
+        data = {"reply_text": "Great Blog anyway"}
+        for _ in range(2):
+            response = self.client.post(
+                path=path,
+                data=data
+            )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_reply_create_un_authenticate(self):
         self.client.credentials(HTTP_AUTHORIZATION=None)
-        response = self.client.delete(path=path)
-        self.assertEqual(response.status_code, 401)
+        path = reverse(
+            'Comment:reply_create',
+            kwargs={'pk': self.comment.pk}
+        )
+        data = {"reply_text": "Great Blog anyway"}
+        response = self.client.post(
+            path=path,
+            data=data
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token))
-        response = self.client.delete(path=path)
-        self.assertEqual(response.status_code, 204)
+    def test_reply_update_status_code(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token)
+        )
+        path = reverse(
+            'Comment:reply_update',
+            kwargs={'pk': self.reply.pk}
+        )
+        data = {"reply_text": "i agree its great edited"}
+        response = self.client.put(
+            path=path,
+            data=data
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_reply_update_content(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token)
+        )
+        path = reverse(
+            'Comment:reply_update',
+            kwargs={'pk': self.reply.pk}
+        )
+        data = {"reply_text": "i agree its great edited"}
+        response = self.client.put(
+            path=path,
+            data=data
+        )
+        self.assertEqual(response.data['reply_text'], data.get('reply_text'))
+
+    def test_reply_update_un_authenticate(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION=None
+        )
+        path = reverse(
+            'Comment:reply_update',
+            kwargs={'pk': self.reply.pk}
+        )
+        data = {"reply_text": "i agree its great edited"}
+        response = self.client.put(
+            path=path,
+            data=data
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_reply_delete_status_code(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user2.access_token)
+        )
+        path = reverse(
+            'Comment:reply_delete',
+            kwargs={'pk': self.reply.pk}
+        )
+        response = self.client.delete(path)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_reply_delete_forbidden(self):
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.jwt_user1.access_token)
+        )
+        path = reverse(
+            'Comment:reply_delete',
+            kwargs={'pk': self.reply.pk}
+        )
+        response = self.client.delete(path)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
